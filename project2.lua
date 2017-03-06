a73382631cf4b8b993dc05e4d4334cbc61757d7c
logan <rs>
2017-03-06 16:00:07 -0700

    ACTUALLY DELETE old file. git does not like zb studio. Remove unused functions in main project

--- project2.lua
+++ project2.lua
@@ -21,33 +21,16 @@ local hProc = require "histogramProc"
 -----------
 
 imageMenu("Point processes", {
-    {"Brighten", pProc.brighten, {{name = "amount", type = "number", displaytype = "spin", default = 0, min = -255, max = 255}}},
     {"Greyscale", pProc.greyscale},
-    {"Negate", pProc.negate},
     {"Threshold", pProc.threshold, {{name = "threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}},
-    {"Contrast Stretch", pProc.contrastStretch, {
-        {name = "rangeStart", type = "number", displaytype = "slider", default = 0, min = 0, max = 255},
-        {name = "rangeEnd", type = "number", displaytype = "slider", default = 255, min = 0, max = 255}}},
-    {"Gamma", pProc.gamma, {{name = "gamma", type = "number", displaytype = "textbox", default = 1, min = 0, max = 25}}},
-    {"Log", pProc.log},
-    {"Bit-Plane Slice", pProc.bitPlaneSlice, {{name = "bit", type = "number", displaytype = "spin", default = 1, min = 1, max = 8}}},
-    {"Posterize", pProc.posterize, {{name = "levels", type = "number", displaytype = "spin", default = 8, min = 2, max = 64}}},
-    {"8 Pseudocolor", pProc.pseudo8},
-    {"Continuous Pseudocolor", pProc.pseudo},
-    {"Solarize", pProc.solarize}
-  })
+  }
+)
 
 imageMenu("Histogram processes", {
     {"Display Histogram", il.showHistogram},
     {"Auto Contrast Stretch", hProc.autoContrastStretch},
-    {"A", il.stretch}, 
-    {"Percentage Contrast Stretch", hProc.percentageContrastStretch, {
-        {name = "lowPercent", type = "number", displaytype = "spin", default = 1, min = 0, max = 100},
-        {name = "highPercent", type = "number", displaytype = "spin", default = 99, min = 0, max = 100}}},
     {"Histogram Equalization", hProc.histogramEqualization},
-    {"Clipped Histogram Equalization", hProc.clippedHistogramEqualization, {{name="clip percent", type = "number", displaytype = "textbox", default = 1}}},
   }
-
 )
 
 imageMenu("Help/About",
