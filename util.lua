e5d4aae5b74da862772b6b25a6c6c85ed5133fc8
Benjamin Garcia <benjamin.garcia@mines.sdsmt.edu>
2017-03-06 13:38:26 -0700

    Added correlation function to util.lua. Need to discuss its return value, currently returns a 2-D array of outputs for the whole image.

--- util.lua
+++ util.lua
@@ -21,7 +21,7 @@ function clip (input, low, high)
   if high == nil then
     high = 255
   end
-  
+
   return math.min(high, math.max(low, input))
 end
 
@@ -42,4 +42,32 @@ function createHistogram (img)
     end
   )
   return histogram
+end
+
+--[[
+  Run a correlation given a kernel and an image to run it on.
+  TODO: Determine if this function should be called with the
+  coordinates in the image, or if it should return a table of
+  return values. currently assuming 2-D array return.
+--]]
+function correlate(img, kernel, operation)
+  local output = {}
+  -- The bounds should prevent indexing past the edges of img.
+  for i = 0 + (kernel.height/2), img.height - (kernel.height/2) do
+    local row = {}
+    for j = 0 + (kernel.width/2), img.width - (kernel.width/2) do
+      row[j] = (
+        local value = 0
+        for k = 0, kernel.height do
+          for l = 0, kernel.width do
+            value += operation(kernel[k][l],
+              img[i-((kernel.height/2)-k)][j-((kernel.width/2)-l)])
+          end
+        end
+        return value
+      )
+    end
+    output[i] = row
+  end
+  return output
 end
