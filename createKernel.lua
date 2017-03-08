22f7b0452878a930f450c8d6d88c137d8de0d25f
Benjamin Garcia <benjamin.garcia@mines.sdsmt.edu>
2017-03-08 10:50:10 -0700

    Added sharpen and median filters to createKernel.lua

--- createKernel.lua
+++ createKernel.lua
@@ -7,22 +7,60 @@
   create a 3x3 smoothing filter.
 --]]
 function smoothingFilter()
-  local im = {}
-  im[0] = {}
-  im[1] = {}
-  im[2] = {}
-  im[0][0] = 1/16
-  im[0][1] = 2/16
-  im[0][2] = 1/16
-  im[1][0] = 2/16
-  im[1][1] = 4/16
-  im[1][2] = 2/16
-  im[2][0] = 1/16
-  im[2][1] = 2/16
-  im[2][2] = 1/16
-  im.width = 3
-  im.height = 3
-  return im
+  local filter = {}
+  filter[0] = {}
+  filter[1] = {}
+  filter[2] = {}
+  filter[0][0] = 1/16
+  filter[0][1] = 2/16
+  filter[0][2] = 1/16
+  filter[1][0] = 2/16
+  filter[1][1] = 4/16
+  filter[1][2] = 2/16
+  filter[2][0] = 1/16
+  filter[2][1] = 2/16
+  filter[2][2] = 1/16
+  filter.width = 3
+  filter.height = 3
+  return filter
+end
+
+function sharpeningFilter()
+  local filter = {}
+  filter[0] = {}
+  filter[1] = {}
+  filter[2] = {}
+  filter[0][0] = 0
+  filter[0][1] = -1
+  filter[0][2] = 0
+  filter[1][0] = -1
+  filter[1][1] = 5
+  filter[1][2] = -1
+  filter[2][0] = 0
+  filter[2][1] = -1
+  filter[2][2] = 0
+  filter.width = 3
+  filter.height = 3
+  return filter
+end
+
+function medianPlusFilter()
+  local filter = {}
+  filter[0] = {}
+  filter[1] = {}
+  filter[2] = {}
+  filter[0][0] = 0
+  filter[0][1] = 1
+  filter[0][2] = 0
+  filter[1][0] = 1
+  filter[1][1] = 1
+  filter[1][2] = 1
+  filter[2][0] = 0
+  filter[2][1] = 1
+  filter[2][2] = 0
+  filter.width = 3
+  filter.height = 3
+  return filter
 end
 
 return {
